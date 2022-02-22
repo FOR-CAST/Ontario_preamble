15a9084209b039f738999f86d7e94e6da9993712
Ian Eddy <ianmseddy@gmail.com>
2022-02-22 15:15:06 -0800

    use prepInputsFarNorthLCC

--- Ontario_preamble.R
+++ Ontario_preamble.R
@@ -349,54 +349,7 @@ Init <- function(sim) {
     ## unable to download directly b/c of SSL, time outs, and other server problems
     ##   https://ws.gisetl.lrc.gov.on.ca/fmedatadownload/Packages/FarNorthLandCover.zip
     ##
-
-    FarNorthLandCoverZip <- file.path(dPath, "FarNorthLandCover.zip")
-    rFiles <- c("New folder/Class/FarNorth_LandCover_Class_UTM15.tif",
-                "New folder/Class/FarNorth_LandCover_Class_UTM16.tif",
-                "New folder/Class/FarNorth_LandCover_Class_UTM17.tif")
-
-    if (!file.exists(FarNorthLandCoverZip)) {
-      googledrive::drive_download(file = as_id("1_QeOAeSHjOjEU1iTqACRj8BYo3ysQKUi"),
-                                  path = FarNorthLandCoverZip)
-      archive::archive_extract(
-        archive = file.path(dPath, "FarNorthLandCover.zip"),
-        dir = file.path(dPath, "FarNorthLandCover"),
-        files = "Version 1.4/TIF Format/ab1da0f2-7bba-430b-af11-d503865ff130-TIF.zip"
-      )
-
-      archive::archive_extract(
-        archive = file.path(dPath, "FarNorthLandCover", "Version 1.4", "TIF Format",
-                            "ab1da0f2-7bba-430b-af11-d503865ff130-TIF.zip"),
-        dir = file.path(dPath, "FarNorthLandCover"),
-        files = rFiles
-      )
-
-      vapply(file.path(dPath, "FarNorthLandCover", rFiles), file.copy, logical(1),
-             to = file.path(dPath, "FarNorthLandCover"))
-      unlink(file.path(dPath, "FarNorthLandCover", "New folder"), recursive = TRUE)
-      unlink(file.path(dPath, "FarNorthLandCover", "Version 1.4"), recursive = TRUE)
-    }
-
-    r15 <- raster(file.path(dPath, "FarNorthLandCover", basename(rFiles))[1])
-    r16 <- raster(file.path(dPath, "FarNorthLandCover", basename(rFiles))[2])
-    r17 <- raster(file.path(dPath, "FarNorthLandCover", basename(rFiles))[3])
-
-    e15 <- extent(r15)
-    e16 <- extent(r16)
-    e17 <- extent(r17)
-    u <- raster::union(e15, e16) %>% raster::union(., e17)
-    template <- raster(u, res = P(sim)$.resolution,  crs = crs(sim$rasterToMatch))
-
-    f <- file.path(dPath, "FarNorthLandCover", paste0("FarNorth_LandCover_Class_UTM", 15:17, ".tif"))
-    t15 <- raster(f[1]) %>% projectRaster(., template, alignOnly = TRUE)
-    t16 <- raster(f[2]) %>% projectRaster(., template, alignOnly = TRUE)
-    t17 <- raster(f[3]) %>% projectRaster(., template, alignOnly = TRUE)
-
-    r15 <- raster(f[1]) %>% projectRaster(., t15, method = "ngb", datatype = "INT2U")
-    r16 <- raster(f[2]) %>% projectRaster(., t16, method = "ngb", datatype = "INT2U")
-    r17 <- raster(f[3]) %>% projectRaster(., t17, method = "ngb", datatype = "INT2U")
-
-    LCC_FN <- raster::mosaic(r15, r16, r17, fun = min)
+    LCC_FN <- prepInputsFarNorthLCC(dPath = dPath)
     LCC_FN[LCC_FN[] <= 0] <- NA_integer_ ## remove 0, -9, and -99
     LCC_FN[LCC_FN[] > 24] <- NA_integer_ ## remove >24
     LCC_FN <- Cache(postProcess, LCC_FN, method = "ngb", rasterToMatch = sim$rasterToMatchLarge)
